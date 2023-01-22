96e24c7bb29764ee6d5249ff92913556cb418253
Sivram Sahu <withsivram@gmail.com>
2023-01-22 15:54:27 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,93 @@
+from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
+import json
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+    new_data = {
+        website: {
+            "email": email,
+                "password": password,
+        }
+    }
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            with open("data.json", "w") as f:
+                json.dump(data, f, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as f:
+                json.dump(data, f, indent = 4)
+        finally:
+            website_input.delete(0, END)
+            password_input.delete(0, END)
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=2, row=1)
+
+website_label = Label(text= "Website :")
+website_label.grid(column=1, row=2)
+
+website_input = Entry(width=55)
+website_input.grid(column=2, row=2, columnspan = 2)
+website_input.focus()
+
+email_label = Label(text= "Email/Username :")
+email_label.grid(column=1, row=3)
+
+email_input = Entry(width=55)
+email_input.grid(column=2, row=3, columnspan = 2)
+email_input.insert(0, "@gmail.com")
+
+password_label = Label(text= "Password :")
+password_label.grid(column=1, row=4)
+
+password_input = Entry(width=36)
+password_input.grid(column=2, row=4)
+
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(column=3, row=4)
+
+add_button = Button(text="Add", width = 47, command=save)
+add_button.grid(column=2, row=5, columnspan = 2)
+
+
+window.mainloop()
